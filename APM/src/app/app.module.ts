dde4aad69054cfeec1e65d66a1b1316dd5f08248
Dani Renner <danijrenner@gmail.com>
2021-06-10 13:56:10 -0700

    add custom pipe to productCode

--- APM/src/app/app.module.ts
+++ APM/src/app/app.module.ts
@@ -3,9 +3,11 @@ import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list.component';
+import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
 
 @NgModule({
-  declarations: [ AppComponent, ProductListComponent ],
+  declarations: [ AppComponent, ProductListComponent,
+  ConvertToSpacesPipe ],
   imports: [ BrowserModule, FormsModule ],
   providers: [],
   bootstrap: [AppComponent]
