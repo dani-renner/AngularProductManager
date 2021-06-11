6f178a201d4bbf3aa16d483a42f08e6fbf90c088
Dani Renner <danijrenner@gmail.com>
2021-06-10 17:26:00 -0700

    add product service

---
+++ APM/src/app/products/product.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { IProduct } from './product';
+
+@Injectable()
+export class ProductService {
+
+  getProducts(): IProduct[] {
+    return [
+      {
+        "productId": 1,
+        "productName": "Leaf Rake",
+        "productCode": "GDN-0011",
+        "releaseDate": "March 19, 2021",
+        "description": "Leaf rake with 48-inch wooden handle.",
+        "price": 19.95,
+        "starRating": 3.2,
+        "imageUrl": "assets/images/leaf_rake.png"
+      },
+      {
+        "productId": 5,
+        "productName": "Hammer",
+        "productCode": "TBX-0048",
+        "releaseDate": "May 21, 2021",
+        "description": "Curved claw steel hammer",
+        "price": 8.9,
+        "starRating": 4.8,
+        "imageUrl": "assets/images/hammer.png"
+      },
+    ]
+  }
+}
