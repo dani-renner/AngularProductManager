a44e9b4373c494618ecbc50db5327f22b95dcbdf
Dani Renner <danijrenner@gmail.com>
2021-06-11 09:26:59 -0700

    import HttpClientModule

--- APM/src/app/app.module.ts
+++ APM/src/app/app.module.ts
@@ -5,11 +5,12 @@ import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list.component';
 import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
 import { StarComponent } from './shared/star.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [ AppComponent, ProductListComponent,
   ConvertToSpacesPipe, StarComponent ],
-  imports: [ BrowserModule, FormsModule ],
+  imports: [ BrowserModule, FormsModule, HttpClientModule ],
   providers: [],
   bootstrap: [AppComponent]
 })
